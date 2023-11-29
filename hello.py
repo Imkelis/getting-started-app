013abc33326d5456641cf5ca4a5b960852ad3b8b
Imkelis <123948448+Imkelis@users.noreply.github.com>
2023-11-29 13:23:30 +0000

    Made my file slightly quirky hello.py

--- hello.py
+++ hello.py
@@ -4,21 +4,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello2():
-    return "Say Hello Ignas from Dockerised Flask"
+    return "<h1 style='color: blue;'>Say Hello Ignas from Dockerised Flask</h1>"
 
 @app.route("/route32a")
 def route32a():
-    return "Hello from the 32A"
+    return "<h2 style='color: green;'>Hello from the 32A</h2>"
 
 @app.route("/route")
 def route():
-    number=request.args.get('number')
-    return "Hello from the {}".format(number)
+    number = request.args.get('number')
+    return f"<h2>Hello from the {number}</h2>"
 
 @app.route("/cc")
 def cc():
-    return "Hello Cloud Computing, this is a Dockerised Flask"
-
+    return "<h2 style='background-color: yellow;'>Hello Cloud Computing, this is a Dockerised Flask</h2>"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port='8080')
+    app.run(host='0.0.0.0', port='8080')
