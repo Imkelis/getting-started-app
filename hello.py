0591995e640cef677c37443ebb9f8b74c696d56d
Imkelis <ignasmerkelisim@gmail.com>
2023-11-29 15:54:18 +0000

    Another final push

--- hello.py
+++ hello.py
@@ -12,22 +12,26 @@ nav_links = '''
 @app.route("/")
 def hello2():
     image_url = url_for('static', filename='image1.png')
-    return f"<body style='background-color: #D15830;'><h1 style='color: blue;'>Say Hello hello my name is ignas from Dockerised Flask</h1><img src='{image_url}'/>" + nav_links + "</body>"
+<<<<<<< HEAD
+    return f"<body style='background-color: #D15830;'><h1 style='color: blue;'>Say GoodBye I hate eveything hello my name is ignas from Dockerised Flask</h1><img src='{image_url}'/>" + nav_links + "</body>"
+=======
+    return f"<body style='background-color: lightblue;'><h1 style='color: blue;'>Say Hello Ignas from Dockerised Flask</h1><img src='{image_url}'/>" + nav_links + "</body>"
+>>>>>>> parent of dfc6e12... finalized custom things, if this doesnt break I think im done hello.py
 
 @app.route("/route32a")
 def route32a():
     image_url = url_for('static', filename='image2.png')
-    return f"<body style='background-color: #1AE2D2;'><h2 style='color: green;'>Hello from the 32A</h2><img src='{image_url}'/>" + nav_links + "</body>"
+    return f"<body style='background-color: lightblue;'><h2 style='color: green;'>Hello from the 32A</h2><img src='{image_url}'/>" + nav_links + "</body>"
 
 @app.route("/route")
 def route():
     image_url = url_for('static', filename='image3.png')
     number = request.args.get('number')
-    return f"<body style='background-color: #57BBD8;'><h2>Hello from the {number}</h2><img src='{image_url}'/>" + nav_links + "</body>"
+    return f"<body style='background-color: lightblue;'><h2>Hello from the {number}</h2><img src='{image_url}'/>" + nav_links + "</body>"
 
 @app.route("/cc")
 def cc():
     image_url = url_for('static', filename='image4.png')
-    return f"<body style='background-color: #C3B08E;'><h2 style='background-color: yellow;'>Hello Cloud Computing, this is a Dockerised Flask</h2><img src='{image_url}'/>" + nav_links + "</body>"
+    return f"<body style='background-color: lightblue;'><h2 style='background-color: yellow;'>Hello Cloud Computing, this is a Dockerised Flask</h2><img src='{image_url}'/>" + nav_links + "</body>"
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8080')
